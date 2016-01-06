170e880a8575361e279b094d02532695bb0b7936
NickNorth <North.N@gmail.com>
2016-01-06 21:03:43 +0000

    More tests

--- Tests/GroupingTests.cs
+++ Tests/GroupingTests.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq;
+﻿using System.Collections.Generic;
+using System.Linq;
 using NUnit.Framework;
 using Shastra.LazyGroupBy;
 
@@ -21,6 +22,40 @@ namespace Tests
             var actual = contents.LazyGroupBy(x => true);
             Assert.That(actual.Count(), Is.EqualTo(1));
             Assert.That(actual.First(), Is.EqualTo(contents));
+        }
+
+        [Test]
+        public void TwoGroupsTest()
+        {
+            var actual = Enumerable.Range(1, 9).LazyGroupBy(x => x / 5);
+            Assert.That(actual.Count(), Is.EqualTo(2));
+            Assert.That(actual.First(), Is.EqualTo(Enumerable.Range(1, 4)));
+            Assert.That(actual.Skip(1).First(), Is.EqualTo(Enumerable.Range(5, 5)));
+        }
+
+        [Test]
+        public void EagerTwoGroupsTest()
+        {
+            var actual = Enumerable.Range(1, 9).LazyGroupBy(x => x / 5).ToList();
+            Assert.That(actual.Count, Is.EqualTo(2));
+            Assert.That(actual.First(), Is.EqualTo(Enumerable.Range(1, 4)));
+            Assert.That(actual.Skip(1).First(), Is.EqualTo(Enumerable.Range(5, 5)));
+        }
+
+        [Test]
+        public void LazyOneGroupTest()
+        {
+            var actual = Integers().LazyGroupBy(x => true).First().First();
+            Assert.That(actual, Is.EqualTo(Integers().First()));
+        }
+
+        private static IEnumerable<int> Integers()
+        {
+            int val = 0;
+            while (true) {
+                yield return val;
+                val++;
+            }
         }
     }
 }
